81a1e14d7329b248ba7488ca8b4d37752c58db54
MRD-HG <roudani.mustapha0@gmail.com>
2025-05-27 23:49:51 +0100

    adding CRUD php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<link rel="stylesheet" type="text/css" href="../css/style.css">
-<h2>Welcome to Student Management System</h2>
-<ul>
-    <li><a href="students/list.php">Manage Students</a></li>
-    <li><a href="courses/list.php">Manage Courses</a></li> 
-    <li><a href="teachers/list.php">Manage Teachers</a></li>
-</ul>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h2>Welcome to Student Management System</h2>
+    <ul class="nav-links"> <li><a href="students/list.php">Manage Students</a></li>
+        <li><a href="courses/list.php">Manage Courses</a></li> 
+        <li><a href="teachers/list.php">Manage Teachers</a></li>
+    </ul>
+</body>
+</html>
